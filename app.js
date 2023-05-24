790c8b71e08d5db03d7c4e96f1c3041198588e76
YumaEndo921 <129018764+YumaEndo921@users.noreply.github.com>
2023-05-24 12:30:46 +0900

    save

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mysql = require('mysql');
 
-const app = express();
+
 
 const connection = mysql.createConnection({
   host: '127.0.0.1', //local host だと進まない
@@ -9,6 +9,8 @@ const connection = mysql.createConnection({
   password: 'cook8921',
   database: 'list_app'
 });
+
+const app = express();
 
 connection.connect((err) => {
   if (err) {
